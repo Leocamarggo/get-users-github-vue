8b5d84104c8a8c926083efaf079b88689e91df81
Leonardo <leonardo.camarggo@outlook.com>
2022-07-19 23:57:33 -0300

    create fetch user request

---
+++ src/service/user.service.ts
@@ -0,0 +1,15 @@
+import store from '@/store/index';
+import api from './api.service'
+
+export default class UserService {
+  static async fetchUser(): Promise<void> {
+    const { userInput } = store.getters;
+    try {
+      const { data } = await api.get(`/users/${userInput}`)
+
+      store.dispatch('updateUserData', data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,35 @@
 <template>
-    <p></p>
+    <input @input="handleChange" :value="userInput" type="text" />
+
+    <p>{{ userData.name }}</p>
+
+    <button @click="fetchUser">Buscar</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { mapGetters, mapActions } from 'vuex';
+import UserService from '@/service/user.service';
 
 export default defineComponent({
-    name: 'HomePage'
+    name: 'HomePage',
+    computed: {
+        ...mapGetters({
+            userData: 'userData',
+            userInput: 'userInput'
+        }),
+    },
+    methods: {
+        ...mapActions({
+            updateUserInput: 'updateUserInput'
+        }),
+        handleChange(event: Event) {
+            this.updateUserInput((event.target as HTMLInputElement).value)
+        },
+        async fetchUser(): Promise<void> {
+            await UserService.fetchUser();
+        },
+    }
 });
 </script>
 
